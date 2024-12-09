a16bdbe13227d2fc7ac0b802b6ac5df8682a53c5
Yogendrasinh Rajput <yogendrasinh@github>
2024-12-10 00:20:49 +0530

    issue with .v12 macOS version in package file

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "SVGKit",
     platforms: [
-        .macOS(.v12),
+        .macOS(.v10_10),
         .iOS(.v13),
         .tvOS(.v13)
     ],
